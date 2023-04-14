d040dd16a852dda1cd63633689b98a0ccab3095b
Sadeghdrr <sadegh.sg.sn@gmail.com>
2023-04-14 21:27:42 +0430

    Add Map

---
+++ Model/map/Texture.java
@@ -0,0 +1,14 @@
+package Model.map;
+
+public enum Texture {
+    SAND,
+    SAND_DUNE,
+    ROCK,       // Unbuildable
+    STONE,
+    IRON,
+    GRASS,
+    DENSE_GRASSLAND,
+    GRASSLAND,  // Unbuildable
+    ;
+
+}
