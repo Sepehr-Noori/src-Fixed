23d4631f8bc5ffb7a3b30b7d4b91117b935a851e
amirrs <ars86677@gmail.com>
2023-04-14 22:53:24 +0330

    GameMenu and its controller added but incomplete

--- Controller/GameMenuController.java
+++ Controller/GameMenuController.java
@@ -1,4 +1,37 @@
 package Controller;
 
+import View.Enums.Messages.GameMenuMessages;
+
+import java.util.regex.Matcher;
+
 public class GameMenuController {
+    private static String showPopularity() {
+        //show popularity factors and show popularity
+        return null;
+    }
+
+    private static String showFoodList() {
+        return null;
+    }
+
+    private static GameMenuMessages checkFoodRate(Matcher matcher) {
+        return null;
+    }
+
+    private static String showFoodRate() {
+        return null;
+    }
+
+    private static GameMenuMessages checkTaxRate(Matcher matcher) {
+
+        return null;
+    }
+
+    private static String showTaxRate() {
+        return null;
+    }
+
+    private static GameMenuMessages checkFearRate(Matcher matcher) {
+        return null;
+    }
 }
--- View/GameMenu.java
+++ View/GameMenu.java
@@ -1,9 +1,43 @@
 package View;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
 
 public class GameMenu {
-    public static void run(Scanner scanner){
+    public static void run() {
+        Scanner scanner;
+    }
+
+    private static void showMap(Matcher matcher) {
+
+    }
+
+    private static void showPopularity() {
+        //show popularity factors and show popularity
+    }
+
+    private static void showFoodList() {
+
+    }
+
+    private static void checkFoodRate(Matcher matcher) {
+
+    }
+
+    private static void showFoodRate() {
+
+    }
+
+    private static void checkTaxRate(Matcher matcher) {
+
+
+    }
+
+    private static void showTaxRate() {
+
+    }
+
+    private static void checkFearRate(Matcher matcher) {
 
     }
 }
