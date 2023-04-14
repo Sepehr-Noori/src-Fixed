a1a359cd48618b4d7182fecd65793e1a38c178b6
amirrs <ars86677@gmail.com>
2023-04-14 17:13:04 +0330

    first

--- Model/Governance.java
+++ Model/Governance.java
@@ -1,9 +1,19 @@
 package Model;
 
-import java.util.ArrayList;
+import Model.Resources.Resource;
+
+import java.util.HashMap;
 
 public class Governance {
+    private final HashMap<Resource, Integer> resource = new HashMap<>();
     private User owner;
-    private static final ArrayList<String> popularityFactors = new ArrayList<>();
-
+    private int gold;
+    private int popularity;
+    private int taxRate;
+    private int foodRate;
+    private int taxFactor;
+    private int fearFactor;
+    private int foodFactor;
+    private int religiousFactor;
+    //popularity = taxFactor + fearFactor + foodFactor + religiousFactor
 }
